22cf1689d15e2879a0f196abb00377636b88272f
hyun <ragnarokdust@gmail.com>
2019-07-19 15:46:11 +0900

    v1

---
+++ test.js
@@ -0,0 +1,23 @@
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
